7306bb696c071b3dfe6dd76118743ab5f27c8f03
Jesus Vazquez <jesvazq.cursos@gmail.com>
2024-12-01 09:35:09 +0100

    primeraLeccion

---
+++ pildoras_informaticas/primerPrograma.py
@@ -0,0 +1,11 @@
+#nuestro primer programa
+mensaje="Hola Python"
+
+print(mensaje)
+print(mensaje)
+
+#Operadores
+# modulo %  facilita el resto de una división
+print("el módulo de 10 % 3 es "+str(10%3))
+
+#luego tenemos exponente ** y división entera // 
---
+++ primerEnsayo.py
@@ -0,0 +1,31 @@
+print ("hello mundo")
+# para hacer comentarios    
+
+# Definimos una variable x con una cadena
+x = "el valor de (a+b)*c es "
+
+# Podemos realizar multiples asignaciones
+a, b, c = 4, 3, 2
+
+# Realizamos unas operaciones con a, b,c
+d=(a+b)*c
+
+#Definimos una variable booleana
+imprimir = True
+
+# si impirimir, print()
+if imprimir:
+    print(x, d)
+
+#Salida: el valor de (a+b)*c es 14
+
+# para comentarios de una linea
+'''para
+comentarios
+de varias lineas'''
+
+# python no usa {} indexa a 4 espacios
+
+'''no hace falta ; al final de la linea
+pero se puede usar para tener varias sentencias en una linea
+x=5; y=10'''
