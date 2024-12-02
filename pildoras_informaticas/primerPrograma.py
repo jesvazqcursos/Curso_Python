d5d53ec55d12e1a90f9684696a10dd350aea0c90
Jesus Vazquez <jesvazq.cursos@gmail.com>
2024-12-02 10:07:39 +0100

    variables

--- pildoras_informaticas/primerPrograma.py
+++ pildoras_informaticas/primerPrograma.py
@@ -9,3 +9,18 @@ print(mensaje)
 print("el módulo de 10 % 3 es "+str(10%3))
 
 #luego tenemos exponente ** y división entera // 
+
+# variables: nombre Nombre Nombre3 mi_nombre nombre_clave_persona
+# type() nos dice el tipo de variable 
+nombre = 5
+print(type(nombre))
+
+##### condicional if
+numero1=5
+numero2=7
+if numero1>numero2:
+    print("el número 1 es mayor que el número 2")
+else:
+    print("el número 2 es mayor")
+
+# -----------Funciones-------------------
